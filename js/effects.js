e18b8beb423ae12bf4290be2555fa749b5b17372
luizcampos <felipe_luiz96@yahoo.com>
2022-08-02 01:39:09 -0300

    add tcc projects

--- js/effects.js
+++ js/effects.js
@@ -45,36 +45,44 @@ btnModes.addEventListener('click', function(){
 //Cards projects
 
 var projects = {
-    title: ['Palette Generator', 'Ignite Lab', 'Quiz HP', 'Calculator', 'API Restful myDreamBox', 'Fazendo Arte'],
+    title: ['Palette Generator', 'Ignite Lab', 'Quiz HP', 'Calculator', 'API Restful myDreamBox', 'Fazendo Arte', 'Monitore', 'Física não é um monstro'],
     imageTop: [
         './img/cards/generator-photo.png',
         './img/cards/ignite-lab-photo.png',
         './img/cards/quiz-hp-photo.png',
         './img/cards/calculator-photo.png',
         './img/cards/mydreambox.png',
-        './img/cards/fazendo-arte-photo.png'],
+        './img/cards/fazendo-arte-photo.png',
+        './img/cards/monitore-photo.png',
+        './img/cards/fneum-photo.png'],
     description: [
         'Um gerador de paleta de cores prático para facilitar a vida dos designers',
         'Projeto feito durante o evento da Rocketseat, com foco em React',
         'Um quiz rápido sobre a história da melhor saga de todos os tempos',
         'Calculadora com as funcionalidades tradicionais e design minimalista',
         'API simples com CRUD completo para uma aplicação de criação de metas financeiras pessoais',
-        'Gerenciamento, conteúdo e estilização do site na plataforma NuvemShop'],
-    status: ['em andamento', 'finalizado', 'finalizado', 'em andamento', 'finalizado', 'finalizado'],
+        'Gerenciamento, conteúdo e estilização do site na plataforma NuvemShop',
+        'Projeto para auxiliar professores na avaliação e no acompanhamento de grupos acadêmicos',
+        'Projeto com app híbrido para mobile e fórum para dúvidas na matéria de física'],
+    status: ['em andamento', 'finalizado', 'finalizado', 'em andamento', 'finalizado', 'finalizado', 'finalizado', 'finalizado'],
     about: [
         'Novas funcionalidades surgirão em breve!',
         'Foi o meu primeiro contato real com ReactJS, o que me motivou ainda mais!',
         'A minha saga favorita foi tema do primeiro projeto pessoal com JavaScript',
         'Novas funcionalidades surgirão em breve!',
         'Apesar de ter sido um desafio de início, consegui desenvolver e captar bastante conteúdo.',
-        'Por ser criado em uma plataforma específica, não tive contato com a criação inicial do código.'],
+        'Por ser criado em uma plataforma específica, não tive contato com a criação inicial do código.',
+        'TCC do curso de graduação Análise e desenvolvimento de sistemas desenvolvido por mim e pelo meu amigo Douglas Kurotaki.',
+        'TCC do curso técnico em informática para auxiliar os alunos no estudo da física.'],
     techs: [
         ['HTML5', 'CSS3', 'JavaScript'],
         ['HTML5', 'CSS3', 'JavaScript', 'React'],
         ['HTML5', 'CSS3', 'JavaScript'],
         ['HTML5', 'CSS3', 'JavaScript'],
         ['HTML5', 'CSS3', 'PHP', 'Laravel'],
-        ['HTML5', 'CSS3']
+        ['HTML5', 'CSS3'],
+        ['HTML5', 'CSS3', 'JavaScript', 'C#', 'MySQL'],
+        ['HTML5', 'CSS3', 'JavaScript', 'PHP', 'MySQL']
     ],
     linkWeb: [
         'https://palette-generator-kappa.vercel.app/',
@@ -82,7 +90,9 @@ var projects = {
         'https://quiz-hp-wine.vercel.app/',
         'https://calculator-inky-zeta.vercel.app/',
         '',
-        'https://fazendoarteonline.com.br'
+        'https://fazendoarteonline.com.br',
+        '',
+        ''
         ],
     linkGitub: [
         'https://github.com/luizcampos/palette-generator',
@@ -90,7 +100,9 @@ var projects = {
         'https://github.com/luizcampos/quiz-hp',
         'https://github.com/luizcampos/calculator',
         'https://github.com/luizcampos/my-dream-box-api',
-        '']
+        '',
+        'https://github.com/luizcampos/monitore',
+        'https://github.com/luizcampos/fisica-nao-e-um-monstro']
 }
 
 const areaCards = document.querySelector('#cardsArea');
@@ -118,6 +130,10 @@ for(var i = 0; i < projects.title.length; i++){
             newDiv += `<i class="fa-brands fa-css3-alt fa-2x animationIcons" title="${projects.techs[i][j]}"></i>`
         } else if(projects.techs[i][j] === 'JavaScript'){
             newDiv += `<i class="fa-brands fa-js-square fa-2x animationIcons" title="${projects.techs[i][j]}"></i>`
+        } else if(projects.techs[i][j] === 'MySQL'){
+            newDiv += `<i class="fa-solid fa-database fa-2x animationIcons" title="${projects.techs[i][j]}"></i>`
+        } else if(projects.techs[i][j] === 'C#'){
+            newDiv += `<i class="fa-solid fa-1x animationIcons" title="${projects.techs[i][j]}"><strong>C#</strong></i>`
         } else{
             newDiv += `<i class="fa-brands fa-${projects.techs[i][j].toLowerCase()} fa-2x animationIcons" title="${projects.techs[i][j]}"></i>`
         }
